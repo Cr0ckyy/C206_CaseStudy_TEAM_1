69d3e5e7369ab647766d7a2b17e69dc086f85113
BuildTools <unconfigured@null.spigotmc.org>
2020-08-15 20:00:20 +0800

    added Course objects, add, view and delete function

--- C206_CaseStudy/src/Course.java
+++ C206_CaseStudy/src/Course.java
@@ -1,4 +1,26 @@
 
-public class Course {
+public class Course extends CourseCategory{
 
+	private int courseCode;
+	private String courseTitle;
+	private String courseDescription;
+	private int courseDuration;
+	private String prerequisiteCourse;
+	
+	public Course(int courseCode, String courseTitle, String courseName, String courseDescription, int courseDuration, String orerequisiteCourse) {
+		this.courseCode = courseCode;
+		this.courseTitle = courseTitle;
+		super(courseName);
+		this.courseDescription = courseDescription;
+		this.courseDuration = courseDuration;
+		this.prerequisiteCourse = prerequisiteCourse;
+	}
+	
+	public void viewCourse() {
+		
+	}
+	
+	public void deleteCourse(int courseCode) {
+		
+	}
 }
