5d57cb82257c0cf9b6da4abe706e49721752d838
BuildTools <unconfigured@null.spigotmc.org>
2020-08-20 10:04:43 +0800

    comment out codes to improve later

--- C206_CaseStudy/src/Course.java
+++ C206_CaseStudy/src/Course.java
@@ -7,17 +7,20 @@ public class Course extends CourseCategory{
 	private int courseDuration;
 	private String prerequisiteCourse;
 	
-	public Course(int courseCode, String courseTitle, String courseName, String courseDescription, int courseDuration, String orerequisiteCourse) {
+	public Course(int courseCode, String courseTitle, String courseCategory, String courseDescription, int courseDuration, String orerequisiteCourse) {
 		this.courseCode = courseCode;
 		this.courseTitle = courseTitle;
-		super(courseName);
+//		super(courseCategory);
 		this.courseDescription = courseDescription;
 		this.courseDuration = courseDuration;
 		this.prerequisiteCourse = prerequisiteCourse;
 	}
 	
 	public void viewCourse() {
-		
+//		String output = "";
+//		output += String.format("%-12s %-20s %-25s %-30s %-15s %s", 
+//				"Course ID", "Course Title", "Course Category", "Course Description", "Course Duration", "Prerequisite Course");
+//		
 	}
 	
 	public void deleteCourse(int courseCode) {
