9d122dae1943a397c3fae6f355ec8a76ea45d97d
BuildTools <unconfigured@null.spigotmc.org>
2020-08-20 10:40:24 +0800

    added getter

--- C206_CaseStudy/src/Course.java
+++ C206_CaseStudy/src/Course.java
@@ -1,13 +1,14 @@
 
-public class Course extends CourseCategory{
+public class Course extends CourseCategory {
 
 	private int courseCode;
 	private String courseTitle;
 	private String courseDescription;
 	private int courseDuration;
 	private String prerequisiteCourse;
-	
-	public Course(int courseCode, String courseTitle, String courseCategory, String courseDescription, int courseDuration, String orerequisiteCourse) {
+
+	public Course(int courseCode, String courseTitle, String courseCategory, String courseDescription,
+			int courseDuration, String orerequisiteCourse) {
 		this.courseCode = courseCode;
 		this.courseTitle = courseTitle;
 //		super(courseCategory);
@@ -15,15 +16,26 @@ public class Course extends CourseCategory{
 		this.courseDuration = courseDuration;
 		this.prerequisiteCourse = prerequisiteCourse;
 	}
-	
-	public void viewCourse() {
-//		String output = "";
-//		output += String.format("%-12s %-20s %-25s %-30s %-15s %s", 
-//				"Course ID", "Course Title", "Course Category", "Course Description", "Course Duration", "Prerequisite Course");
-//		
+
+	public int getCourseCode() {
+		return courseCode;
 	}
-	
-	public void deleteCourse(int courseCode) {
-		
+
+	public String getCourseTitle() {
+		return courseTitle;
 	}
+
+	public String getCourseDescription() {
+		return courseDescription;
+	}
+
+	public int getCourseDuration() {
+		return courseDuration;
+	}
+
+	public String getPrerequisiteCourse() {
+		return prerequisiteCourse;
+	}
+
 }
+// JU LONG
