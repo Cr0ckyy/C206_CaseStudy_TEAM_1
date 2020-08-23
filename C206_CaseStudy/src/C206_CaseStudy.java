aa902cb060464fb1f21ff78e958e7d649aaecf83
19037610 <19037610@myrp.edu.sg>
2020-08-23 20:31:25 +0800

    Edited deleteCourseSchedule

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -726,31 +726,6 @@ public class C206_CaseStudy {
 		}
 		String output = doDeleteCourseSchedule(scheduleList, regiList, scheduleID);
 		System.out.println(output);
-
-//		String output = "";
-//
-//		if (scheduleList.size() == 0) {
-//			output += "There are no Course Schedules to delete";
-//		} else {
-//			viewCourseSchedule();
-//			int deleteIdx = -1;
-//			int scheduleID = Helper.readInt("Enter Schedule ID: ");
-//
-//			for (int i = 0; i < scheduleList.size(); i++) {
-//				if (scheduleList.get(i).getCSid() == scheduleID) {
-//					deleteIdx = i;
-//				}
-//			}
-//
-//			for (Register i : regiList) {
-//				if (i.getCSid() == scheduleID) {
-//					deleteIdx = -2;
-//				}
-//			}
-//			
-//			output += doDeleteCourseSchedule(scheduleList, regiList, deleteIdx);
-//			
-//		}
 	}
 
 	// Alicia
